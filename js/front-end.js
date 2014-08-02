d9d1e4edb1086cfc6a944e45cd3ddd90f8cd358d
lexoyo <a.hoyau@silexlabs.org>
2014-08-02 13:35:28 +0200

    css effects and new pic

--- js/front-end.js
+++ js/front-end.js
@@ -10,6 +10,10 @@
 //////////////////////////////////////////////////
 
 $(function() {
+  // allow HTML5 tags used by Silex to be styled with CSS (polyfill)
+  document.createElement('HEADER');
+  document.createElement('VIDEO');
+
   /**
    * list all pages from the head section
    * and open the 1st one by default
@@ -25,7 +29,6 @@ $(function() {
    * called when a page is opened
    */
   $('body').on('pageChanged', function (event, pageName) {
-    console.log(arguments);
     // mark links to the current page as active
     $('[data-silex-href="#!'+pageName+'"]').addClass('page-link-active');
   });
@@ -36,11 +39,13 @@ $(function() {
     currentPage: firstPageName,
     useDeeplink:true,
     pageClass: 'paged-element',
+    window: window.top || window
   });
   /**
    * silex links
+   * only when the classname .silex-runtime is defined on the body (not while editing)
    */
-  $('[data-silex-href]').click(function () {
+  $('.silex-runtime [data-silex-href]').click(function () {
     var href = this.getAttribute('data-silex-href');
     if (href.indexOf('#!') === 0){
       window.location.href = href;
@@ -49,24 +54,4 @@ $(function() {
       window.open(href, '_blank');
     }
   });
-  /**
-   * resize body to the size of its content
-   */
-  function onResize(event){
-    var width = 0;
-    var height = 0;
-    $(".background").each(function (index) {
-      var position = $(this).position();
-      var right = position.left + $(this).width();
-      var bottom = position.top + $(this).height();
-      if (width < right) width = right;
-      if (height < bottom) height = bottom;
-    });
-    $("body").css({
-      "min-width": width + "px",
-      "min-height": height + "px"
-    });
-  }
-  // call it at start
-  onResize();
 })
