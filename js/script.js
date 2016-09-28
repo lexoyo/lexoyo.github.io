a66523eb53ab4ab24caedd000c434fd47e7d1bce
Alex Hoyau <a.hoyau@silexlabs.org>
2016-09-28 21:56:26 +0200

    Create js/script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 
         $(function() {
-            $('.big-image').click(function() {
+            $('.tof').click(function() {
                 $('.big-image')
                     .animate({top: '99%'}, 200, function() {
                         $('.big-image')
                         .css('background-image', "url('assets/from-small.jpg')")
-                        .animate({top: '0'}, 200, function() {
+                        .animate({top: '0'}, 1000, function() {
                         });
                     })
                 console.log('aaa');
